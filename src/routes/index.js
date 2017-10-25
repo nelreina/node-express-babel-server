20b0e8a633fef8b964db5fdb24f7c4369804661f
Nelreina <nelreina@gmail.com>
2017-10-25 03:12:36 -0400

    added util files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,16 @@
 import fs from 'fs';
 import path from 'path';
 
-export default (app) => {
-	console.log('Importing routes...');
-	fs.readdirSync(__dirname)
-	.filter(function(file) {
-		return (file.indexOf(".") !== 0) && (file !== "index.js");
-	})
-	.forEach(function(file) {
-		const routepath = file.replace('.js', '');
-		console.log(routepath);
-		app.use(`/api/${routepath}`,	require(path.join(__dirname, file)));
-	});
-}
+export default app => {
+  console.log('Importing routes...');
+  fs
+    .readdirSync(__dirname)
+    .filter(function(file) {
+      return file.indexOf('.') !== 0 && file !== 'index.js';
+    })
+    .forEach(function(file) {
+      const routepath = file.replace('.js', '');
+      console.log(`/api/${routepath}`);
+      app.use(`/api/${routepath}`, require(path.join(__dirname, file)));
+    });
+};
