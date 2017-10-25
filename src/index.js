c45f900a86a9fbd7d40df5dbb0d6dbeb875d976b
Nelreina <nelreina@gmail.com>
2017-10-25 03:00:41 -0400

    upgrade packages and addrd spread object babel plugin

--- src/index.js
+++ src/index.js
@@ -14,48 +14,56 @@ import config from './config.json';
 const app = express();
 const port = config.port || 3000;
 
-
 app.use(compression());
-app.use(session({
+app.use(
+  session({
     secret: 'node',
     resave: false,
-    saveUninitialized: false,
-}));
+    saveUninitialized: false
+  })
+);
 app.use(cors());
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.'),
+        root = namespace.shift(),
+        formParam = root;
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
+      while (namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
     }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
+  })
+);
 
 console.log('Starting Server...');
 
 routes(app);
 
-models.sequelize.sync({ }).then( () => {
-	
-	// Not Found Routes
-	app.all('/*', (req, res)=>{
-		res.status(401).send({message: 'Not authorized'})
-	});
-	
-	
-	const server = http.createServer(app);
-	
-	server.listen(port, () => console.log(`App is served on port ${port}`))
+// models.sequelize.sync({ }).then( () => {
 
+// Ping Server
+const response = { message: 'Server is alive and well!', status: 'OK' };
+app.get('/ping', (req, res) => {
+  res.send({ ...response, status: 'Algo mas' });
 });
+
+// Not Found Routes
+app.all('/*', (req, res) => {
+  res.status(401).send({ message: 'Not authorized' });
+});
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`App is served on port ${port}`));
+
+// });
